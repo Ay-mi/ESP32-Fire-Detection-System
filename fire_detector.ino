5c106e0dd521d2873b51c5e9fffef6abedb5261e
Ameenah <150582455+Ay-mi@users.noreply.github.com>
2025-06-21 22:16:25 +0800

    Update fire_detector.ino

--- fire_detector.ino
+++ fire_detector.ino
@@ -14,8 +14,12 @@
 #define DHTTYPE DHT11      //Either DHT11
 #define CO2_PIN 34         // MQ sensor
 #define LDR_PIN 35         //Light sensor
-#define PUMP_PIN 26        //Water pump //this is not going to work like this
+#define PUMP_RELAY_PIN 26        //Water pump //this is not going to work like this
 #define BUZZER_PIN 25      //buzzer
+
+#define LED1 16
+#define LED2 17
+#define LED3 18
 
 DHT dht(DHTPIN, DHTTYPE);
 #define INTERVAL 3000            // Interval at which to send sensor data (milliseconds)
@@ -23,7 +27,7 @@ unsigned long previousMillis = 0; // Stores last time sensor data was sent
 
 char auth[] = BLYNK_AUTH_TOKEN;  // Blynk authorization token
 char ssid[] = "Redmi_Note_14";                // WiFi SSID
-char pass[] = "a6gqjkm67c78mdk";                // WiFi password
+char pass[] = "xxx";                // WiFi password
 
 void setup() {
   Serial.begin(115200);
@@ -34,13 +38,36 @@ void setup() {
   pinMode(CO2_PIN, INPUT);
   pinMode(LDR_PIN, INPUT);  
   pinMode(BUZZER_PIN, OUTPUT);
-  pinMode(PUMP_PIN, OUTPUT);
-  
-  // Ensure all actuators are off initially
-  digitalWrite(BUZZER_PIN, LOW);
-  digitalWrite(PUMP_PIN, LOW);
+  pinMode(PUMP_RELAY_PIN, OUTPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+
   Blynk.begin(auth, ssid, pass); // Connect to Blynk
   Serial.println("System ready. Starting sensor monitoring...");
+
+  //testing system
+  digitalWrite(LED1, HIGH);
+  delay(500);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, HIGH);
+  delay(500);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, HIGH);
+  delay(500);
+  digitalWrite(LED3, LOW);
+  digitalWrite(BUZZER_PIN, HIGH);
+  delay(200);
+  digitalWrite(BUZZER_PIN, LOW); 
+  delay(300);
+  digitalWrite(BUZZER_PIN, HIGH);
+  delay(200);
+  digitalWrite(BUZZER_PIN, LOW);
+  //end of test
+
+  digitalWrite(BUZZER_PIN, LOW);
+  digitalWrite(PUMP_RELAY_PIN, LOW);
+
 }
 void loop() {
   int fireAlert = 0;
@@ -66,7 +93,7 @@ void loop() {
 
   //LDR
   int ldrValue = analogRead(LDR_PIN);
-  Serial.print("\n Light Intensity: ");
+  Serial.print("\nLight Intensity: ");
   Serial.println(ldrValue);
   Blynk.virtualWrite(V2, ldrValue);          // Send LDR reading to Blynk
 
@@ -77,25 +104,70 @@ void loop() {
   Serial.println(co2Value);
 
   //make decision
-  fireAlert = decision(co2Value, ldrValue, t, h);
+  decision(co2Value, ldrValue, t, h);
   Serial.print("Fire Alert: ");
   Serial.println(fireAlert);
+  delay(10000);
 }}
 
 //decision function
-//since dht11 is not reading, we'll only use mq2 and ldr values for now
-int decision(int co2, int ldr, float t, float hum){
+void decision(int co2, int ldr, float t, float hum){
   int co2Value = co2;
   int ldrValue = ldr;
   float temp = t;
   float humidity = hum;
   int fireAlert = 0;
-  if(co2Value > 3000 || ldrValue > 3000 || temp > 120 || humidity < 20){
+  if(co2Value > 3000 || ldrValue > 4000 || temp > 120 || humidity < 20){
     fireAlert = 2;
   }
-  else if(co2Value > 2500 || ldrValue > 2500 || temp > 100 || humidity < 40){
+  else if(co2Value > 2500 || ldrValue > 3500 || temp > 70 || humidity < 40){
     fireAlert = 1;
   }
-  return fireAlert;
-}
 
+  //LED Control
+  if(temp > 70 || humidity < 40){
+    digitalWrite(LED1, HIGH);
+  }
+  if(co2Value > 2500){
+    digitalWrite(LED2, HIGH);
+  }
+  if(ldrValue > 3500){
+    digitalWrite(LED3, HIGH);
+  }
+
+  //Water pump and buzzer control
+  if(fireAlert == 2){
+    Serial.print("LEDs ON\n");
+    digitalWrite(BUZZER_PIN, HIGH);   // Activate buzzer
+    Serial.println("Buzzer activated");
+    delay(5000);
+    digitalWrite(BUZZER_PIN, LOW);    // Deactivate
+    Serial.println("Buzzer deactivated");
+    digitalWrite(PUMP_RELAY_PIN, HIGH);
+    Serial.println("Water pump activated");
+    delay(10000);
+    digitalWrite(PUMP_RELAY_PIN, LOW);
+    Serial.println("Water pump deactivated");
+    delay(5000);
+
+  }
+  else if(fireAlert == 1){
+    Serial.print("LEDs ON\n");
+    digitalWrite(BUZZER_PIN, LOW);    // Deactivate
+    Serial.println("Buzzer deactivated");
+    digitalWrite(PUMP_RELAY_PIN, LOW);
+    Serial.println("Water pump deactivated");
+    delay(5000);
+  }
+  else{
+    Serial.print("LEDs OFF\n");
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+    digitalWrite(BUZZER_PIN, LOW);    // Deactivate
+    Serial.println("Buzzer deactivated");
+    digitalWrite(PUMP_RELAY_PIN, LOW);
+    Serial.println("Water pump deactivated");
+    delay(5000);
+  }
+}
